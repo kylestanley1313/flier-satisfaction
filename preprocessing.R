a8e64b2f22eea70597aa42adaedf9052c29a031b
kylestanley1313 <kylestanley1313@gmail.com>
2021-11-01 18:58:35 -0400

    removes irrelevant columns from data files

--- preprocessing.R
+++ preprocessing.R
@@ -1,5 +1,9 @@
 data <- read.csv("full.csv", header = TRUE)
 
+## Remove irrelevant columns
+data <- data[, -which(names(data) %in% c('X.1','X'))]
+
+## Train-test split
 N <- nrow(data)
 n.train <- round(0.75*N)
 n.test <- N - n.train
