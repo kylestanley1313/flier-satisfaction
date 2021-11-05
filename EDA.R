7cffc4e9dcb6ab3be92022ab93076aa3b33b94ef
chaegeun <schg8603@gmail.com>
2021-11-05 16:00:31 -0400

    draw plots to use

--- EDA.R
+++ EDA.R
@@ -40,39 +40,57 @@ p <- ggplot(temp, aes(satisfaction,))
 p1 <- p+geom_bar();p1
 
 ## categorical variables vs satisfaction
-p2 <- p+geom_bar(aes(fill=Type.of.Travel));p2
-p3 <- p+geom_bar(aes(fill=Class));p3
-p9 <- p+geom_bar(aes(fill=Online.boarding));p9
-p10 <- p+geom_bar(aes(fill=Cleanliness));p10
-p11 <- p+geom_bar(aes(fill=Food.and.drink));p11
-p12 <- p+geom_bar(aes(fill=Seat.comfort));p12
-grid.arrange(p2,p3,p9,p11, nrow=2, ncol=2)
+p2 <- p+geom_bar(aes(fill=Type.of.Travel),position="fill");p2
+p3 <- p+geom_bar(aes(fill=Class),position="fill");p3
+p9 <- p+geom_bar(aes(fill=Online.boarding),position="fill");p9
+p10 <- p+geom_bar(aes(fill=Cleanliness),position="fill");p10
+p11 <- p+geom_bar(aes(fill=Food.and.drink),position="fill");p11
+p12 <- p+geom_bar(aes(fill=Seat.comfort),position="fill");p12
+#grid.arrange(p2,p3,p9,p11, nrow=2, ncol=2)
 
 ## continuous variables vs satisfaction
 p5 <- p+geom_boxplot(aes(satisfaction,Departure.Delay.in.Minutes));p5
 p6 <- p+geom_boxplot(aes(satisfaction,Arrival.Delay.in.Minutes));p6
 p7 <- p+geom_boxplot(aes(satisfaction,Flight.Distance));p7
 p8 <- p+geom_boxplot(aes(satisfaction,Age));p8
-grid.arrange(p5,p6,p7,p8, nrow=2, ncol=2)
+#grid.arrange(p5,p6,p7,p8, nrow=2, ncol=2)
 
 ## mixed 2 by 2 panel
-grid.arrange(p3,p11,p7,p8, nrow=2, ncol=2)
+plot1 <- grid.arrange(p3,p11,p7,p8, nrow=2, ncol=2);plot1
 
 ## predictors -------------------------------------------
 ## conti vs conti
-ggplot(temp, aes(Flight.Distance, Arrival.Delay.in.Minutes))+
-  geom_point(aes(color=satisfaction))
+# ggplot(temp, aes(Flight.Distance, Arrival.Delay.in.Minutes))+
+#   geom_point(aes(color=satisfaction))
 
-ggplot(temp, aes(Departure.Delay.in.Minutes, Arrival.Delay.in.Minutes))+
-  geom_point(aes(color=satisfaction))
+plot2 <- ggplot(temp, aes(Departure.Delay.in.Minutes, Arrival.Delay.in.Minutes))+
+  geom_point(aes(color=satisfaction));plot2
 
 ## cat vs cat
 tb <- table(temp$Online.boarding, temp$Inflight.wifi.service)
 tb <- as.data.frame(tb) 
 colnames(tb)=c("Online.boarding", "Inflight.wifi.service", "Freq")
 
-ggplot(tb, aes(Online.boarding, Freq,
-                 fill=Inflight.wifi.service))+
-  geom_bar(stat = 'identity', position = 'dodge')
+# plot3 <- ggplot(tb, aes(Online.boarding, Freq,
+#                  fill=Online.boarding))+
+#   geom_bar(stat = 'identity', position = 'dodge')+
+#   facet_wrap(~Inflight.wifi.service);plot3
 
-## cat vs conti
+plot3 <- ggplot(tb, aes(Online.boarding, Freq,
+                        fill=Inflight.wifi.service))+
+  geom_bar(stat = 'identity', position = 'dodge')+
+  facet_wrap(~Inflight.wifi.service);plot3
+
+
+## Plot to use ----------------------------------------
+grid.arrange(p3,p11,p7,p8, nrow=2, ncol=2)
+grid.arrange(plot2, plot3, nrow=2, ncol=1)
+
+## correlation mat
+
+
+
+
+
+
+
