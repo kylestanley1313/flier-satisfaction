32ac40e6dbe130aa38f81b18d2458598cdb690b3
chaegeun <schg8603@gmail.com>
2021-11-30 22:26:19 -0500

    K- means PCA

--- clustering/kmeans.R
+++ clustering/kmeans.R
@@ -139,7 +139,7 @@ plot(n.values, ARI3,
      main="Adjusted rand index(ARI) with k=3")
 
 # When k=4 ------------------------------------------------ 
-# Fix k=5 and change nstart(initial assignment)
+# Fix k=4 and change nstart(initial assignment)
 k <- 4
 n.values <- 1:15
 
@@ -165,18 +165,241 @@ plot(n.values, ARI4,
      ylab="ARI values",
      main="Adjusted rand index(ARI) with k=4")
 
+# When k=5 ------------------------------------------------ 
+# Fix k=5 and change nstart(initial assignment)
+k <- 5
+n.values <- 1:15
+
+cluster.output <- function(nstart){
+  kmeans(x, centers = k, nstart)$cluster
+}
+
+# get each cluster result
+set.seed(1)
+cluster.values <- lapply(n.values, cluster.output)
+
+# calculate adjusted rand index(ARI)
+ARI5 <- rep(NA, max(n.values))
+for(i in n.values){
+  ARI5[i] <- adj.rand.index(trueclass, cluster.values[[i]])
+}
+ARI5
+
+# plot the ARI
+plot(n.values, ARI5,
+     type="b", pch = 19, frame = FALSE, 
+     xlab="Number of initial assignment",
+     ylab="ARI values",
+     main="Adjusted rand index(ARI) with k=5")
+
+# When k=6 ------------------------------------------------ 
+# Fix k=6 and change nstart(initial assignment)
+k <- 6
+n.values <- 1:15
+
+cluster.output <- function(nstart){
+  kmeans(x, centers = k, nstart)$cluster
+}
+
+# get each cluster result
+set.seed(1)
+cluster.values <- lapply(n.values, cluster.output)
+
+# calculate adjusted rand index(ARI)
+ARI6 <- rep(NA, max(n.values))
+for(i in n.values){
+  ARI6[i] <- adj.rand.index(trueclass, cluster.values[[i]])
+}
+ARI6
+
+# plot the ARI
+plot(n.values, ARI6,
+     type="b", pch = 19, frame = FALSE, 
+     xlab="Number of initial assignment",
+     ylab="ARI values",
+     main="Adjusted rand index(ARI) with k=6")
+
 ## Compare
-mean(ARI2);mean(ARI3);mean(ARI4)
+mean(ARI2);mean(ARI3);mean(ARI4);mean(ARI5);mean(ARI6);
+
+###############################################################-
+## PCA                                                     ####
+###############################################################-
+X.pca <- prcomp(x, center = TRUE, scale. = TRUE, rank. = 2)
+x.pca <- X.pca$x
+
+
+
+# When k=2 ------------------------------------------------
+# Fix k=2 and change nstart(initial assignment)
+k <- 2
+n.values <- 1:15
+
+cluster.output <- function(nstart){
+  kmeans(x.pca, centers = k, nstart)$cluster
+}
+
+# get each cluster result
+set.seed(1)
+cluster.values <- lapply(n.values, cluster.output)
+
+# calculate adjusted rand index(ARI)
+ARI2 <- rep(NA, max(n.values))
+for(i in n.values){
+  ARI2[i] <- adj.rand.index(trueclass, cluster.values[[i]])
+}
+ARI2
+
+# plot the ARI
+plot(n.values, ARI2,
+     type="b", pch = 19, frame = FALSE, 
+     xlab="Number of initial assignment",
+     ylab="ARI values",
+     main="Adjusted rand index(ARI) with k=2")
+
+# When k=3 ------------------------------------------------ 
+# Fix k=3 and change nstart(initial assignment)
+k <- 3
+n.values <- 1:15
+
+cluster.output <- function(nstart){
+  kmeans(x.pca, centers = k, nstart)$cluster
+}
+
+# get each cluster result
+set.seed(1)
+cluster.values <- lapply(n.values, cluster.output)
+
+# calculate adjusted rand index(ARI)
+ARI3 <- rep(NA, max(n.values))
+for(i in n.values){
+  ARI3[i] <- adj.rand.index(trueclass, cluster.values[[i]])
+}
+ARI3
+
+# plot the ARI
+plot(n.values, ARI3,
+     type="b", pch = 19, frame = FALSE, 
+     xlab="Number of initial assignment",
+     ylab="ARI values",
+     main="Adjusted rand index(ARI) with k=3")
+
+# When k=4 ------------------------------------------------ 
+# Fix k=4 and change nstart(initial assignment)
+k <- 4
+n.values <- 1:15
+
+cluster.output <- function(nstart){
+  kmeans(x.pca, centers = k, nstart)$cluster
+}
+
+# get each cluster result
+set.seed(1)
+cluster.values <- lapply(n.values, cluster.output)
+
+# calculate adjusted rand index(ARI)
+ARI4 <- rep(NA, max(n.values))
+for(i in n.values){
+  ARI4[i] <- adj.rand.index(trueclass, cluster.values[[i]])
+}
+ARI4
+
+# plot the ARI
+plot(n.values, ARI4,
+     type="b", pch = 19, frame = FALSE, 
+     xlab="Number of initial assignment",
+     ylab="ARI values",
+     main="Adjusted rand index(ARI) with k=4")
+
+# When k=5 ------------------------------------------------ 
+# Fix k=5 and change nstart(initial assignment)
+k <- 5
+n.values <- 1:15
+
+cluster.output <- function(nstart){
+  kmeans(x.pca, centers = k, nstart)$cluster
+}
+
+# get each cluster result
+set.seed(1)
+cluster.values <- lapply(n.values, cluster.output)
+
+# calculate adjusted rand index(ARI)
+ARI5 <- rep(NA, max(n.values))
+for(i in n.values){
+  ARI5[i] <- adj.rand.index(trueclass, cluster.values[[i]])
+}
+ARI5
+
+# plot the ARI
+plot(n.values, ARI5,
+     type="b", pch = 19, frame = FALSE, 
+     xlab="Number of initial assignment",
+     ylab="ARI values",
+     main="Adjusted rand index(ARI) with k=5")
+
+# When k=6 ------------------------------------------------ 
+# Fix k=6 and change nstart(initial assignment)
+k <- 6
+n.values <- 1:15
+
+cluster.output <- function(nstart){
+  kmeans(x.pca, centers = k, nstart)$cluster
+}
+
+# get each cluster result
+set.seed(1)
+cluster.values <- lapply(n.values, cluster.output)
+
+# calculate adjusted rand index(ARI)
+ARI6 <- rep(NA, max(n.values))
+for(i in n.values){
+  ARI6[i] <- adj.rand.index(trueclass, cluster.values[[i]])
+}
+ARI6
+
+# plot the ARI
+plot(n.values, ARI6,
+     type="b", pch = 19, frame = FALSE, 
+     xlab="Number of initial assignment",
+     ylab="ARI values",
+     main="Adjusted rand index(ARI) with k=6")
+
+
+## Compare
+mean(ARI2);mean(ARI3);mean(ARI4);mean(ARI5);mean(ARI6)
+
 
 ## visualization ---------------------------------------
-k2 <- kmeans(x, centers = 2, nstart = 10)
-k3 <- kmeans(x, centers = 3, nstart = 10)
-k4 <- kmeans(x, centers = 4, nstart = 10)
 
-p2 <- fviz_cluster(k2, geom = "point",  data = x) + ggtitle("k = 2")
-p3 <- fviz_cluster(k3, geom = "point",  data = x) + ggtitle("k = 3")
-p4 <- fviz_cluster(k4, geom = "point",  data = x) + ggtitle("k = 4")
-
-grid.arrange(p2, p3, p4, nrow = 2)
+k2.pca <- kmeans(x.pca, centers = 2, nstart = 10)
+k3.pca <- kmeans(x.pca, centers = 3, nstart = 10)
+k4.pca <- kmeans(x.pca, centers = 4, nstart = 10)
+k5.pca <- kmeans(x.pca, centers = 5, nstart = 10)
+k6.pca <- kmeans(x.pca, centers = 6, nstart = 10)
 
 
+par(mfrow=c(2,3))
+plot(x.pca, col=trueclass)
+plot(x.pca, col=k2.pca$cluster)
+plot(x.pca, col=k3.pca$cluster)
+plot(x.pca, col=k4.pca$cluster)
+plot(x.pca, col=k5.pca$cluster)
+plot(x.pca, col=k6.pca$cluster)
+
+# p2.pca <- fviz_cluster(k2.pca, geom = "point",  data = x) + ggtitle("k = 2")
+# p3.pca <- fviz_cluster(k3.pca, geom = "point",  data = x) + ggtitle("k = 3")
+# p4.pca <- fviz_cluster(k4.pca, geom = "point",  data = x) + ggtitle("k = 4")
+# 
+# grid.arrange(p2.pca, p3.pca, p4.pca, nrow = 2)
+
+# k2 <- kmeans(x, centers = 2, nstart = 10)
+# k3 <- kmeans(x, centers = 3, nstart = 10)
+# k4 <- kmeans(x, centers = 4, nstart = 10)
+# 
+# p2 <- fviz_cluster(k2, geom = "point",  data = x) + ggtitle("k = 2")
+# p3 <- fviz_cluster(k3, geom = "point",  data = x) + ggtitle("k = 3")
+# p4 <- fviz_cluster(k4, geom = "point",  data = x) + ggtitle("k = 4")
+# 
+# grid.arrange(p2, p3, p4, nrow = 2)
+# 
